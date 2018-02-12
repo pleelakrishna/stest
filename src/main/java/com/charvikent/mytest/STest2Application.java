ca5b3195c958cd15ca4daaa07911bc9f30539cfe
unknown <leelakrishnacce@gmail.com>
2018-02-12 21:55:26 +0530

    spring security project addeded

---
+++ src/main/java/com/charvikent/mytest/STest2Application.java
@@ -0,0 +1,12 @@
+package com.charvikent.mytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class STest2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(STest2Application.class, args);
+	}
+}
